fdac064b5028542669feff2390cfa15c4c8806c4
olawanle temitayo <olawanletemitayo@gmail.com>
2023-03-08 10:35:56 +0100

    feat: added a preloader to entice users

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,29 @@
-import { GlobalStyle } from '@/components/General/GlobalStyle';
+import { GlobalStyle } from '@/utils/GlobalStyle';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ThirdwebProvider } from '@thirdweb-dev/react';
 import Moralis from 'moralis';
+import { useEffect, useState } from 'react';
+import Preloader from '@/components/General/Preloader';
 
 Moralis.start({
   apiKey: process.env.NEXT_PUBLIC_MORALIS_API_KEY,
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
+  // a preloader can be added here
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 4000);
+  }, []);
   const activeChain = 'ethereum';
   return (
     <>
       <GlobalStyle />
+      <Preloader loading={loading} />
       <ThirdwebProvider activeChain={activeChain}>
         <Component {...pageProps} />
       </ThirdwebProvider>
