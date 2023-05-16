9f52e738a9653e8ce526b2c42e93d345215dab92
Temmy <olawanletemitayo@gmail.com>
2023-05-16 23:46:22 +0100

    Update GlobalStyle.ts

--- utils/GlobalStyle.ts
+++ utils/GlobalStyle.ts
@@ -1,12 +1,12 @@
 import { createGlobalStyle, GlobalStyleComponent } from 'styled-components';
 
 export const GlobalStyle: GlobalStyleComponent<{}, {}> = createGlobalStyle`
-    :root {
-        // font family
-        --primary-font: 'Poppins', sans-serif;
-        --secondary-font: 'SPACE EXPLORER', sans-serif; 
+   :root {
+       // font family
+       --primary-font: 'Poppins', sans-serif;
+       --secondary-font: 'SPACE EXPLORER', sans-serif; 
         
-        // color
-        --primary-color: #9D8E8E;
-    }
+       // color
+       --primary-color: #9D8E8E;
+   }
 `;
